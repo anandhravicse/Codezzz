4ba7aae4cb2b085b0d5eafceb1e77d7b29fe9368
anandhravicse <30927070+anandhravicse@users.noreply.github.com>
2017-08-11 17:03:38 +0530

    Create vowelDemo.java

---
+++ vowelDemo.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*
+class vowelDemo
+   {
+   public static void main(String as[])
+      {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the letter");
+      Char c=sc.next.charAt();
+      if((c==a)||(c==e)||(c==i)||(c==o)||(c==u))
+          {
+           System.out.println("the given letter is vowel");
+           }
+        else
+           System.out.println("the given letter is consonents");
+      }
+    }
+ 
