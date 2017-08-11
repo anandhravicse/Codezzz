06b24c6a8510a6f815ffb78006255691bfba1af0
anandhravicse <30927070+anandhravicse@users.noreply.github.com>
2017-08-11 16:42:11 +0530

    Create evenDemo.java

---
+++ evenDemo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class evenDemo
+   {
+     public static void main(String as[])
+        {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter the number");
+         int num=sc.nextInt();
+         if(num%2==0)
+             {
+             System.out.println("the given number is even");
+              }
+          else
+             {
+             System.out.println("the given number is odd");
+             
+              }
+         }
+    }
