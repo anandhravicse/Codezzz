5529a54de4b024e8b22de894204890af14fee7ba
anandhravicse <30927070+anandhravicse@users.noreply.github.com>
2017-08-11 16:35:25 +0530

    Create numDemo.java

---
+++ numDemo.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class numDemo
+   {
+   public static void main(String as[])
+        {
+          Scanner sc=new Scanner(System.in);
+          System.out.println("enter  the number");
+          int num=sc.nextInt();
+          if(num>0)
+             {
+              System.out.println("the given number is positive number");
+              
+              }
+           else if(num<0)
+               {
+               System.out.println("the given number is negative number");
+               
+               }
+            else
+               {
+               system.out.println("the number is zero");
+               }
+        }
+   
+   }
